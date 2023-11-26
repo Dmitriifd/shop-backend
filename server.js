c3547f2dbda844895530bd020cc56bee1e597b9d
Dmitriifd <idimka9@gmail.com>
2023-11-26 18:54:51 +0500

    connect MongoDB

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-import express from "express";
-import dotenv from "dotenv";
-import products from "./data/products.js";
-dotenv.config()
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products.js';
+dotenv.config();
 
 const port = process.env.PORT || 5000;
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
 
 app.get('/api/products', (req, res) => {
   res.json(products);
-})
+});
 
 app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
-})
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
+});
