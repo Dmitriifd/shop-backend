78babab0d072f3239f7e3dfb4bede8e029619cee
Dmitriifd <idimka9@gmail.com>
2023-11-28 18:11:20 +0500

    getUserProfile

--- controllers/userController.js
+++ controllers/userController.js
@@ -82,7 +82,19 @@ const logoutUser = (req, res) => {
  * @access  Private
  */
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.json('Get user profile');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 /**
