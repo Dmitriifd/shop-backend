96b4e25c22b94b601b66611114f2b116197d3fc6
Dmitriifd <idimka9@gmail.com>
2023-11-28 16:58:29 +0500

    generateToken

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 /**
  * @desc    Auth user & get token
@@ -12,6 +13,8 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
     res.json({
       _id: user._id,
       name: user.name,
