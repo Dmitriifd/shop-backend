0f0c811c91ceba0b348a9d66adfeaede0be24e7b
Dmitriifd <idimka9@gmail.com>
2023-11-28 17:13:23 +0500

    authMiddleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -8,8 +9,13 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 connectDB();
+
 const app = express();
+
 app.use(express.json()); // req.body parser
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
